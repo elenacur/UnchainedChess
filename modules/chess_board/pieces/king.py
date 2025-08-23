37c8c3086285a02319e81d24694044468292040d
elenacur <elena.currah@gmail.com>
2025-08-23 16:41:19 +0100

    King legal moves

--- modules/chess_board/pieces/king.py
+++ modules/chess_board/pieces/king.py
@@ -50,5 +50,18 @@ class King(Piece):
         self.__can_move = p_can_move
 
     #other methods
-    def get_legal_moves(self):
-        None
+    def get_legal_moves(self, colour, pieces):
+        legal_moves = []
+        king_moves = [(1, 1), (1, -1), (-1, 1), (-1, -1), (0, 1), (0, -1), (1, 0), (-1, 0)] #ways a king can move  
+        for i in range(0, 8):
+            friendly_fire = False #so it can't take a piece of the same colour
+            legal_row = self.get_row() + king_moves[i][0]
+            legal_column = self.get_column() + king_moves[i][1]
+            if 0 <= legal_row <= 7 and 0 <= legal_column <= 7:
+                if pieces[legal_row][legal_column] != None:
+                    if pieces[legal_row][legal_column].get_colour() == colour: 
+                        friendly_fire = True
+                if friendly_fire == False:
+                    #adding this move to the legal_moves list if it's in range and isn't moving to a piece of the same colour
+                    legal_moves.append([legal_row, legal_column])
+        return legal_moves
