0bfa6f1529b1bb78e32d193e5273ac9fba206de8
elenacur <elena.currah@gmail.com>
2025-12-18 00:30:08 +0000

    comments

--- prototypes/file_handling.py
+++ prototypes/file_handling.py
@@ -1,10 +1,10 @@
 
-pgn = "1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5"
+pgn = "1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5" #example pgn
 
-file = open("file_prototype.txt", "w")
-file.write(pgn)
+file = open("file_prototype.txt", "w") #open file
+file.write(pgn) #write the pgn to the file
 
-file = open("file_prototype.txt", "r")
-print(file.read())
+file = open("file_prototype.txt", "r") 
+print(file.read()) #print contents of file for testing
 
-file.close()
+file.close() #close file
--- prototypes/pieces_list.py
+++ prototypes/pieces_list.py
@@ -1,15 +1,18 @@
+
+#mock of 2D pieces array
 pieces = [["r", "n", "b", "q", "k", "b", "n", "r"], ["p", "p", "p", "p", "p", "p", "p", "p"], 
          ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], 
          ["", "", "", "", "", "", "", ""], ["p", "p", "p", "p", "p", "p", "p", "p"], 
          ["r", "n", "b", "q", "k", "b", "n", "r"]]
 
-notation = []
+notation = [] #list of moves
 
+#adds a move to notation
 def notate_move(current_row, current_column, new_row, new_column):
     move = pieces[current_row][current_column] + str(new_column) + str(new_row)
     notation.append(move)
 
-
+#moves piece to new index of pieces
 def move_piece(current_row, current_column, new_row, new_column):
     notate_move(current_row, current_column, new_row, new_column)
     pieces[new_row][new_column] = pieces[current_row][current_column]
@@ -19,7 +22,7 @@ def move_piece(current_row, current_column, new_row, new_column):
         print(i)
     
 
-
+#test moves
 move_piece(6, 4, 4, 4)
 print(notation)
 move_piece(1, 4, 3, 4)
